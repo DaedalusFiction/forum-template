d1ad0d25b0ca7e022de3e517acc6f6eace316606
David Sorensen <teardownthismall@gmail.com>
2022-06-24 17:55:18 -0400

    initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,70 @@
+import { Box, Button, Container, Typography } from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+    const links = [
+        {
+            text: "FDA's PFDD Information",
+            href: "https://www.fda.gov/drugs/development-approval-process-drugs/cder-patient-focused-drug-development",
+        },
+        {
+            text: "Kennedy's Disease Association",
+            href: "https://kennedysdisease.org",
+        },
+    ];
+    const navigateToTop = () => {
+        window.scrollTo(0, 0);
+    };
+    return (
+        <Box
+            sx={{
+                backgroundColor: "var(--bg-accent)",
+                padding: "2rem 0",
+            }}
+        >
+            <Container>
+                <Box
+                    sx={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        alignItems: "end",
+                    }}
+                >
+                    <Box>
+                        <Typography
+                            variant="h6"
+                            sx={{ color: "white", marginBottom: "1em" }}
+                        >
+                            Kennedy's Disease Association 2022 PFDD
+                        </Typography>
+                        {links.map((item, index) => {
+                            return (
+                                <a
+                                    key={index}
+                                    href={item.href}
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <Typography
+                                        sx={{ color: "var(--fc-gray-200)" }}
+                                        gutterBottom
+                                    >
+                                        {item.text}
+                                    </Typography>
+                                </a>
+                            );
+                        })}
+                    </Box>
+                    <Link to="/">
+                        <Button onClick={navigateToTop} sx={{ color: "white" }}>
+                            Back to Top
+                        </Button>
+                    </Link>
+                </Box>
+            </Container>
+        </Box>
+    );
+};
+
+export default Footer;
---
+++ src/components/Profile.js
@@ -0,0 +1,127 @@
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+    decrement,
+    increment,
+    incrementByAmount,
+    incrementAsync,
+    incrementIfOdd,
+    selectCount,
+} from "../features/counter/counterSlice";
+import {
+    updateCurrentUser,
+    selectUser,
+    selectSpotifyToken,
+} from "../features/user/userSlice";
+import {
+    Avatar,
+    Box,
+    Button,
+    IconButton,
+    Menu,
+    MenuItem,
+    Tooltip,
+    Typography,
+} from "@mui/material";
+import {
+    getAuth,
+    GoogleAuthProvider,
+    onAuthStateChanged,
+    signInWithPopup,
+} from "firebase/auth";
+import { auth, provider } from "../firebase";
+
+const settings = ["Settings", "Logout"];
+
+const Profile = () => {
+    const [currentUser, setCurrentUser] = useState(null);
+    const count = useSelector(selectCount);
+    const user = useSelector(selectUser);
+    const spotifyToken = useSelector(selectSpotifyToken);
+    const dispatch = useDispatch();
+    const [incrementAmount, setIncrementAmount] = useState("2");
+
+    const incrementValue = Number(incrementAmount) || 0;
+
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+    useEffect(() => {
+        const auth = getAuth();
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                // User is signed in, see docs for a list of available properties
+                // https://firebase.google.com/docs/reference/js/firebase.User
+                const uid = user.uid;
+                setCurrentUser(user);
+                // ...
+            } else {
+                // User is signed out
+                // ...
+            }
+        });
+        return () => {};
+    }, []);
+
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+        // console.log(user);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    const handleMenuClick = (e) => {
+        switch (e.target.innerHTML) {
+            case "Settings":
+                console.log(spotifyToken);
+                setAnchorElUser(null);
+                break;
+            case "Logout":
+                auth.signOut().then(dispatch(updateCurrentUser(null)));
+                setAnchorElUser(null);
+                break;
+            default:
+                return;
+        }
+    };
+
+    return (
+        <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                    <Avatar
+                        alt={user ? user.name : ""}
+                        src={
+                            user ? user.photoURL : "/static/images/avatar/2.jpg"
+                        }
+                    />
+                </IconButton>
+            </Tooltip>
+            <Menu
+                sx={{ mt: "45px" }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+            >
+                {settings.map((setting) => (
+                    <MenuItem key={setting} onClick={handleMenuClick}>
+                        <Typography textAlign="center">{setting}</Typography>
+                    </MenuItem>
+                ))}
+            </Menu>
+        </Box>
+    );
+};
+
+export default Profile;
